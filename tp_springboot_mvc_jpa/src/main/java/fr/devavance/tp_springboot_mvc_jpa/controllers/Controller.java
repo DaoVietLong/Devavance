7379fe9048106d1e368c8e2fcd317ae966f31165
DaoVietLong <daovietlong10@gmail.com>
2025-03-04 17:05:16 +0100

    initialize controller and create method to return template1

---
+++ tp_springboot_mvc_jpa/src/main/java/fr/devavance/tp_springboot_mvc_jpa/controllers/Controller.java
@@ -0,0 +1,11 @@
+package fr.devavance.tp_springboot_mvc_jpa.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @RequestMapping("/")
+    public String getTemplate1() {
+        return "template1";
+    }
+}

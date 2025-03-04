4b335ed12d2ce9a3a7e08f80acd8ecb49553e7cf
DaoVietLong <daovietlong10@gmail.com>
2025-03-04 18:14:19 +0100

    initialize interface IEmployeeDAO with method add employee to array and method findAll to return employee array and class EmployeeDAOImpl implements IEmployeeDAO

--- tp_springboot_mvc_jpa/src/main/java/fr/devavance/tp_springboot_mvc_jpa/dao/IEmployeeDAO.java
+++ tp_springboot_mvc_jpa/src/main/java/fr/devavance/tp_springboot_mvc_jpa/dao/IEmployeeDAO.java
@@ -1,4 +1,10 @@
 package fr.devavance.tp_springboot_mvc_jpa.dao;
 
+import fr.devavance.tp_springboot_mvc_jpa.beans.Employee;
+
+import java.util.List;
+
 public interface IEmployeeDAO {
+    List<Employee> findAll();
+    public void add(Employee employee);
 }
